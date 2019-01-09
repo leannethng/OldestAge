33cab1f34c734517a5cc1010a153ab8c1b1782b3
Leanne <leanne.thng@gmail.com>
2019-01-08 19:43:08 -0800

    Added table row names and cleaned rows and columns

--- OldAge.R
+++ OldAge.R
@@ -12,6 +12,13 @@ webpage
 webpage_table <- html_table(html_nodes(webpage, "table")[[1]], fill = TRUE)
 webpage_table
 
+#Cleaning 
+table_cleaned_columns <- webpage_table[-c(1:8, 74:118), ]
+table_cleaned_rows <- table_cleaned_columns[-c(1,18:22)]
+
+rownames(table_cleaned_rows) <- 1:nrow(table_cleaned_rows)
+colnames(table_cleaned_rows) <- c("Birthplace","Name", "Born", "Died", "Age - Years", "Age - Days", "Race", "Sex", "Deathplace", "When Oldest - Years Range", "When Oldest - Age Range", "Length of Reign - Years", "Length of Reign - Days", "Reign Length - In Years", "Age at Accession - Years", "Age at Asccession - Days")
+
 
 
 
